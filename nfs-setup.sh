b47226704f8c1f3247ab21b16fb6c5aae1e100d9
Milos Puric <milos.puric@softwire.com>
2024-09-18 15:50:27 +0200

    Working NFS server in docker

---
+++ nfs-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# exit if command fails
+set -e
+
+# all directories to be exported
+mounts="${@}"
+
+echo "#NFS Exports" > /etc/exports
+
+# create and export the directories
+for mnt in "${mounts[@]}"; do
+  src=$(echo $mnt | awk -F ':' '{ print $1 }')
+  mkdir -p $src
+  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+done
+
+# run all the scripts - init,stop
+exec runsvdir /etc/sv
