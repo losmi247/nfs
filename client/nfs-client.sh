0910593770481b06eddadea19e07529138e583bb
Milos <milospuric856@gmail.com>
2024-10-08 16:34:15 +0100

    fsdfs

--- client/nfs-client.sh
+++ client/nfs-client.sh
@@ -5,6 +5,17 @@ export NFS_PORT_2049_TCP_ADDR=192.168.100.1
 
 set -e
 
+# enable NFSv2 on 22.04
+# echo -e "[nfsd]\nudp=n\ntcp=y\nvers2=y\nvers3=y\nvers4=y" | tee /etc/nfs.conf.d/local.conf
+
+# enable NFSv2 and disable NFSv3 and NFSv4
+# crudini --set /etc/nfs.conf nfsd vers2 y
+# crudini --set /etc/nfs.conf nfsd vers3 n
+# crudini --set /etc/nfs.conf nfsd vers4 n
+# enable tcp and udp
+# crudini --set /etc/nfs.conf nfsd tcp y
+# crudini --set /etc/nfs.conf nfsd udp y
+
 # mount NFS server directories onto client host
 mounts="${@}"
 targets=()
@@ -23,7 +34,7 @@ for mnt in "${mounts[@]}"; do
   mkdir -p $client_target
   # mount the server directory on client target (file system type is nfs)
   # ('mount -t type device dir' mounts the device of type type at the directory dir)
-  mount --types nfs --options proto=tcp,port=2049 ${NFS_PORT_2049_TCP_ADDR}:${server_source} ${client_target}
+  mount --types nfs --options proto=tcp,port=2049,vers=2 ${NFS_PORT_2049_TCP_ADDR}:${server_source} ${client_target}
 done
 
 echo "Monitoring changes in mounted directories..."
